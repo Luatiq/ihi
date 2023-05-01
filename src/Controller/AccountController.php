c8ba0ac8e053d739953ce8b9d23f04510f0eb762
Lua van Loon <lua@luati.cc>
2023-05-01 19:07:01 +0200

    added skeleton for register/login forms & /logout route

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+#[Route('/account', name: 'account')]
+class AccountController extends AbstractController
+{
+    private UserRepository $repository;
+
+    public function __construct(
+        UserRepository $repository
+    ) {
+        $this->repository = $repository;
+    }
+
+    #[Route('/register', name: '.register')]
+    public function register(
+        ?User $entity,
+        UserPasswordHasherInterface $passwordHasher,
+        Request $request
+    ): Response {
+        if (!$entity) $entity = new User();
+
+        $form = $this->createForm(UserType::class, $entity);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPass = $passwordHasher->hashPassword($entity, $entity->getPassword());
+            $entity->setPassword($hashedPass);
+
+            $this->repository->save($entity, true);
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/login', name: '.login')]
+    public function login(
+        AuthenticationUtils $authenticationUtils
+    ): Response {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the account
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'lastUsername' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: '.logout')]
+    public function logout(): RedirectResponse {
+        // Is never called, security.yaml overrides
+        return $this->redirectToRoute('account.login');
+    }
+
+    #[Route('/', name: '.view', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+}
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,7 @@ class UserType extends AbstractType
             ->add('username')
             // @TODO render roles, only when role > ROLE_USER, not yourself
 //            ->add('roles')
-            ->add('password')
+            ->add('password', PasswordType::class)
         ;
     }
 
